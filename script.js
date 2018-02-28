991cc62a8b3f554adc0c6c977cb2130957ef82b1
Tomasz Sokolowski <sokolowskit@gmail.com>
2018-02-28 23:27:29 +0100

    Update js file

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var arrayMale = ['Tom', 'Mark', 'Bob', 'Robert', 'Dominic', 'Stephen'],
 	arrayFemale = ['Maggie', 'Sabrina', 'Laura', 'Eve', 'Natalie', 'Jenny'],
-	arrayMaleFemale = arrayMale.concat(arrayFemale),
+	bothArrays = arrayMale.concat(arrayFemale),
 	newName1 = 'Gilbert',
 	newName2 = 'Natalie',
 	newName3 = 'Nancy',
@@ -11,13 +11,13 @@ console.log('Male array = ' + arrayMale);
 
 console.log('Female array = ' + arrayFemale);
 
-console.log('Both arrays as one = ' + arrayMaleFemale);
+console.log('Both arrays as one = ' + bothArrays);
 
 for (i=1; i<5; i++) {
-	var foundIndexName = arrayMaleFemale.indexOf(window['newName' + i]);
+	var foundIndexName = bothArrays.indexOf(window['newName' + i]);
 	if (foundIndexName === -1) {
-		arrayMaleFemale.push(window['newName' + i]);
-		console.log('New set of array values = ' + arrayMaleFemale);
+		bothArrays.push(window['newName' + i]);
+		console.log('New set of array values = ' + bothArrays);
 	} else {
 		console.log('Provided name is already present in array.');
 	}
